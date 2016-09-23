fcdcaca4e089a091b674d6a1534b8816c1b2273c
Fernando Cruz <f.cruzval@gmail.com>
2016-09-23 23:00:52 +0000

    Added user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   has_one :profile
   has_many :jumps
   has_many :comments
+  
+  has_secure_password
+  
+  validates :email, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#welcome'
+  
+  get '/register' => 'users#new'
+  post '/register' => 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
